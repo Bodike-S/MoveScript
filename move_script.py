e80b6c58e8e9d385be423679825de7d38d77bd99
BodikeS <bodike@pixward.com>
2023-12-22 11:50:08 +0200

    Add import dronekit_sitl

--- move_script.py
+++ move_script.py
@@ -1,3 +1,7 @@
+import dronekit_sitl
+sitl = dronekit_sitl.start_default()
+connection_string = sitl.connection_string()
+
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 import time
 import math
